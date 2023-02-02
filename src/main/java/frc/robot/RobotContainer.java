273d81ddfe394880a3e970928ebb70acf13cf3d5
programming 2 <makeshiftrobotics@gmail.com>
2023-02-01 20:52:59 -0500

    added gripper subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -50,11 +50,16 @@ public class RobotContainer {
       new JoystickButton(operator, XboxController.Button.kX.value);
   private final JoystickButton backButton = 
       new JoystickButton(operator, XboxController.Button.kLeftBumper.value);
+  private final JoystickButton leftTrigger =
+      new JoystickButton(operator, XboxController.Button.kLeftTrigger.value);
+  private final JoystickButton rightTrigger =
+      new JoystickButton(operator, XboxController.Button.kRightTrigger.value);
      
   
   /* Subsystems */
   private final Swerve s_Swerve = new Swerve();
   private final Pivot s_Pivot = new Pivot();
+  private final Gripper s_Gripper = new Gripper();
 
   public class setDefaultCommand{}
 
@@ -90,6 +95,8 @@ public class RobotContainer {
     bButton.onTrue(new InstantCommand(() -> s_Pivot.goToHorizontal()));
     xButton.onTrue(new InstantCommand(() -> s_Pivot.goToScoring()));
     backButton.onTrue(new InstantCommand(()-> s_Pivot.setZero()));
+    leftTrigger.onTrue(new InstantCommand(()-> s_Gripper.setForward()));
+    rightTrigger.onTrue(new InstantCommand(()-> s_Gripper.setReverse()));
   }
 
 
--- src/main/java/frc/robot/subsystems/Gripper.java
+++ src/main/java/frc/robot/subsystems/Gripper.java
@@ -1,14 +1,29 @@
-// package frc.robot.subsystems;
+package frc.robot.subsystems;
 
-// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// import frc.robot.Constants;
-// import com.revrobotics.RelativeEncoder;
-// //import frc
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
-// public class Gripper extends SubsystemBase {
+public class Gripper extends SubsystemBase {
 
-//     public Gripper(){
+    DoubleSolenoid gripperDoublePCM = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
 
-//     }
-// }
+    public Gripper(){
+    }
+
+    public void setOff(){
+        gripperDoublePCM.set(DoubleSolenoid.Value.kOff);
+    }
+
+    public void setForward(){
+        gripperDoublePCM.set(DoubleSolenoid.Value.kForward);
+    }
+
+    public void setReverse(){
+        gripperDoublePCM.set(DoubleSolenoid.Value.kReverse);
+    }
+}
