821fc73e15ffe62b5494cf42b9f1275bfb3c9b02
Pablo Cafiso <pablo.cafiso@verticalmotion.ca>
2023-03-11 10:48:51 -0500

    Code tidying up

--- src/main/java/frc/robot/PhotonCameraWrapper.java
+++ src/main/java/frc/robot/PhotonCameraWrapper.java
@@ -29,7 +29,6 @@ import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj.DriverStation;
-import frc.robot.Constants.VisionConstants;
 import java.io.IOException;
 import java.util.Optional;
 import org.photonvision.EstimatedRobotPose;
