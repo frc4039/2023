6182cec74cabe2c9bbcde43046fb396cb23d6061
Noah Linton <nlinton123@hotmail.com>
2023-03-11 14:01:44 -0500

    removed pivot before release
    Driver RB will open without dropping first

--- src/main/java/frc/robot/commands/CmdGrpGamePieceScoring.java
+++ src/main/java/frc/robot/commands/CmdGrpGamePieceScoring.java
@@ -18,7 +18,7 @@ public class CmdGrpGamePieceScoring extends SequentialCommandGroup {
 
     public CmdGrpGamePieceScoring(Pivot s_Pivot, Gripper s_Gripper, Telescopic s_Telescopic) {
         addCommands(
-                new PivotMoveToPosition(s_Pivot, PivotConstants.kPositionScoringRelease),
+                // new PivotMoveToPosition(s_Pivot, PivotConstants.kPositionScoringRelease),
                 new GripperRelease(s_Gripper).withTimeout(GripperConstants.kGripperReleaseTimeout),
                 new ParallelCommandGroup(new Command[] {
                         new TelescopicRetract(s_Telescopic),
