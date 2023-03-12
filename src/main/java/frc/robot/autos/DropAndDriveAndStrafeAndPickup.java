854138cd5b0ac08e2eac9ed249d8c72590ef42a5
maisy thomas <maisy0404@outlook.com>
2023-03-12 13:49:37 -0400

    strafe adjustments

--- src/main/java/frc/robot/autos/DropAndDriveAndStrafeAndPickup.java
+++ src/main/java/frc/robot/autos/DropAndDriveAndStrafeAndPickup.java
@@ -32,14 +32,42 @@ public class DropAndDriveAndStrafeAndPickup extends SequentialCommandGroup {
                         new TelescopicRetract(container.getTelescopic()).withTimeout(1.0),
                         new PivotMoveToPosition(container.getPivot(), Constants.PivotConstants.kPositionTravel)
                                 .withTimeout(1.0) }) }));
-        addCommands(new ResetRobotPose(container.getSwerve(), backoutPath_step0.getInitialPose()));
-        addCommands(AutoFollowPath.createFollowCommand(container.getSwerve(), backoutPath_step0));
-        
+        addCommands(new ResetRobotPose(container.getSwerve(), strafePath_step1.getInitialPose()));
+        addCommands(AutoFollowPath.createFollowCommand(container.getSwerve(), strafePath_step1));
+        addCommands(new ParallelCommandGroup(new CmdGrpConePickupPosition(container.getTelescopic(), container.getGripper(), container.getConeGuide(), container.getPivot(), container.getIntake()),
+                    AutoFollowPath.createFollowCommand(container.getSwerve(), returnPath_1)));
+        addCommands(new GripperRetrieve(container.getGripper())
+                .withTimeout(Constants.GripperConstants.kGripperReleaseTimeout));
+        addCommands(new ParallelCommandGroup( new CmdGrpTravelPosition(container.getTelescopic(), container.getConeGuide(), container.getPivot(), container.getIntake()),
+                    AutoFollowPath.createFollowCommand(container.getSwerve(), returnPath_2)));
+        addCommands(new PivotMoveToPosition(container.getPivot(), Constants.PivotConstants.kPositionScoringCone));
+        addCommands(new TelescopicScoringExtendMid(container.getTelescopic(), container.getPivot()).withTimeout(1.0));
+        addCommands(new SequentialCommandGroup(new Command[] {
+                    new PivotMoveToPosition(container.getPivot(), Constants.PivotConstants.kPositionScoringRelease),
+                    new GripperRelease(container.getGripper())
+                            .withTimeout(Constants.GripperConstants.kGripperReleaseTimeout),
+                    new ParallelCommandGroup(new Command[] {
+                    new TelescopicRetract(container.getTelescopic()).withTimeout(1.0),
+                    new PivotMoveToPosition(container.getPivot(), Constants.PivotConstants.kPositionTravel)
+                            .withTimeout(1.0) }) }));;
     }
 
-    public static Trajectory backoutPath_step0 = TrajectoryGenerator.generateTrajectory(
+
+    public static Trajectory strafePath_step1 = TrajectoryGenerator.generateTrajectory(
             new Pose2d(0, 0, Rotation2d.fromDegrees(0)),
-            List.of(),
-            new Pose2d(5, 0, Rotation2d.fromDegrees(0)),
+            List.of(new Translation2d (3.5,0), new Translation2d (5, -0.7), new Translation2d (6, -0.7)),
+            new Pose2d(6, 0.3, Rotation2d.fromDegrees(0)),
             Constants.AutoConstants.forwardConfig);
+
+    public static Trajectory returnPath_1 = TrajectoryGenerator.generateTrajectory(
+                new Pose2d(6, 0.3, Rotation2d.fromDegrees(0)),
+                List.of(),
+                new Pose2d(4.5, 0.3, Rotation2d.fromDegrees(0)),
+                Constants.AutoConstants.reverseConfig);
+
+    public static Trajectory returnPath_2 = TrajectoryGenerator.generateTrajectory(
+                new Pose2d(4.5, 0.3, Rotation2d.fromDegrees(0)),
+                List.of(),
+                new Pose2d(0, 0, Rotation2d.fromDegrees(0)),
+                Constants.AutoConstants.reverseConfig);
 }
