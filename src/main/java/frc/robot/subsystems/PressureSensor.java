e8688e119120410c23433dbe0639d4a91dace941
4039-zach <118681764+4039-zach@users.noreply.github.com>
2023-03-15 18:57:40 -0400

    use canids for compressor

--- src/main/java/frc/robot/subsystems/PressureSensor.java
+++ src/main/java/frc/robot/subsystems/PressureSensor.java
@@ -5,7 +5,8 @@ import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
 public class PressureSensor extends SubsystemBase {
-    static Compressor analogCompressor = new Compressor(1, PneumaticsModuleType.REVPH);
+    static Compressor analogCompressor = new Compressor(7, PneumaticsModuleType.REVPH); // This uses CANIDs, not
+                                                                                        // Pneumatic Hub IDs...
 
     public static double getAnalogPressureReading() {
         return analogCompressor.getPressure();
