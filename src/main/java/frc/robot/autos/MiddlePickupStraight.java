144f26d39d750ef9ac057ebd792f544c186f31a7
maisy thomas <maisy0404@outlook.com>
2023-03-15 18:55:13 -0400

    added blue side

--- src/main/java/frc/robot/autos/MiddlePickupStraight.java
+++ src/main/java/frc/robot/autos/MiddlePickupStraight.java
@@ -11,6 +11,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
@@ -21,7 +22,8 @@ import edu.wpi.first.wpilibj2.command.Command;
 public class MiddlePickupStraight extends SequentialCommandGroup {
 
     public MiddlePickupStraight(RobotContainer container) {
-        addCommands(new ResetRobotPose(container.getSwerve(), middlePath_1.getInitialPose()));
+        if (DriverStation.getAlliance().toString() == "Red") {
+        addCommands(new ResetRobotPose(container.getSwerve(), middlePath_1_Red.getInitialPose()));
 
         // Pivot and extend arm
         addCommands(new PivotMoveToPosition(container.getPivot(), Constants.PivotConstants.kPositionScoringCone));
@@ -39,7 +41,7 @@ public class MiddlePickupStraight extends SequentialCommandGroup {
                 new PivotMoveToPosition(container.getPivot(), Constants.PivotConstants.kPositionGreenCubePickup),
                 // new TelescopicGreenCube(container.getTelescopic()).withTimeout(1.0),
                 AutoFollowPath.createFollowCommand(container.getSwerve(),
-                        middlePath_1) }));
+                        middlePath_1_Red) }));
 
         // close gripper
         addCommands(new GripperRetrieve(container.getGripper())
@@ -48,7 +50,7 @@ public class MiddlePickupStraight extends SequentialCommandGroup {
         // scoring position and drive back to score
         addCommands(new ParallelCommandGroup(
                 new CmdGrpScoringPosition(container.getConeGuide(), container.getTelescopic(), container.getPivot()),
-                AutoFollowPath.createFollowCommand(container.getSwerve(), returnPath_1)));
+                AutoFollowPath.createFollowCommand(container.getSwerve(), returnPath_1_Red)));
         // addCommands(new PivotMoveToPosition(container.getPivot(),
         // Constants.PivotConstants.kPositionScoringCone));
 
@@ -64,23 +66,87 @@ public class MiddlePickupStraight extends SequentialCommandGroup {
                         new TelescopicRetract(container.getTelescopic()).withTimeout(1.0),
                         new PivotMoveToPosition(container.getPivot(), Constants.PivotConstants.kPositionTravel)
                                 .withTimeout(1.0),
-                        AutoFollowPath.createFollowCommand(container.getSwerve(), middlePath_2) }) }));
+                        AutoFollowPath.createFollowCommand(container.getSwerve(), middlePath_2_Red) }) }));
         ;
+        } else if (DriverStation.getAlliance().toString() == "Blue") {
+        addCommands(new ResetRobotPose(container.getSwerve(), middlePath_1_Blue.getInitialPose()));
+
+        // Pivot and extend arm
+        addCommands(new PivotMoveToPosition(container.getPivot(), Constants.PivotConstants.kPositionScoringCone));
+        addCommands(new TelescopicScoringExtendFar(container.getTelescopic(), container.getPivot()).withTimeout(1.0));
+
+        // drop pivot and open gripper
+        addCommands(
+                new PivotMoveToPosition(container.getPivot(), Constants.PivotConstants.kPositionScoringRelease));
+        addCommands(new GripperRelease(container.getGripper())
+                .withTimeout(Constants.GripperConstants.kGripperReleaseTimeout));
+
+        // retract arm and pivot up to vertical, also start driving to middle
+        addCommands(new ParallelCommandGroup(new Command[] {
+                new TelescopicRetract(container.getTelescopic()).withTimeout(1.0),
+                new PivotMoveToPosition(container.getPivot(), Constants.PivotConstants.kPositionGreenCubePickup),
+                // new TelescopicGreenCube(container.getTelescopic()).withTimeout(1.0),
+                AutoFollowPath.createFollowCommand(container.getSwerve(),
+                        middlePath_1_Blue) }));
+
+        // close gripper
+        addCommands(new GripperRetrieve(container.getGripper())
+                .withTimeout(Constants.GripperConstants.kGripperReleaseTimeout));
+
+        // scoring position and drive back to score
+        addCommands(new ParallelCommandGroup(
+                new CmdGrpScoringPosition(container.getConeGuide(), container.getTelescopic(), container.getPivot()),
+                AutoFollowPath.createFollowCommand(container.getSwerve(), returnPath_1_Blue)));
+        // addCommands(new PivotMoveToPosition(container.getPivot(),
+        // Constants.PivotConstants.kPositionScoringCone));
+
+        // extend arm
+        addCommands(new TelescopicScoringExtendFar(container.getTelescopic(), container.getPivot()).withTimeout(1.0));
+        // Drop score, and retract
+        addCommands(new SequentialCommandGroup(new Command[] {
+                // new PivotMoveToPosition(container.getPivot(),
+                // Constants.PivotConstants.kPositionScoringRelease),
+                new GripperRelease(container.getGripper())
+                        .withTimeout(Constants.GripperConstants.kGripperReleaseTimeout),
+                new ParallelCommandGroup(new Command[] {
+                        new TelescopicRetract(container.getTelescopic()).withTimeout(1.0),
+                        new PivotMoveToPosition(container.getPivot(), Constants.PivotConstants.kPositionTravel)
+                                .withTimeout(1.0),
+                        AutoFollowPath.createFollowCommand(container.getSwerve(), middlePath_2_Blue) }) }));
+            }
     }
 
-    public static Trajectory middlePath_1 = TrajectoryGenerator.generateTrajectory(
+    public static Trajectory middlePath_1_Red = TrajectoryGenerator.generateTrajectory(
             new Pose2d(0, 0, Rotation2d.fromDegrees(0)),
             List.of(),
             new Pose2d(4.65, 0, Rotation2d.fromDegrees(0)),
             Constants.AutoConstants.forwardConfig);
 
-    public static Trajectory returnPath_1 = TrajectoryGenerator.generateTrajectory(
+    public static Trajectory returnPath_1_Red = TrajectoryGenerator.generateTrajectory(
             new Pose2d(4.65, 0, Rotation2d.fromDegrees(0)),
             List.of(new Translation2d(1, 0)),
             new Pose2d(0, 0.5, Rotation2d.fromDegrees(0)),
             Constants.AutoConstants.reverseConfig);
 
-    public static Trajectory middlePath_2 = TrajectoryGenerator.generateTrajectory(
+    public static Trajectory middlePath_2_Red = TrajectoryGenerator.generateTrajectory(
+            new Pose2d(0, 0, Rotation2d.fromDegrees(0)),
+            List.of(new Translation2d(1, 0)),
+            new Pose2d(5, 0, Rotation2d.fromDegrees(0)),
+            Constants.AutoConstants.forwardConfig);
+
+    public static Trajectory middlePath_1_Blue = TrajectoryGenerator.generateTrajectory(
+            new Pose2d(0, 0, Rotation2d.fromDegrees(0)),
+            List.of(),
+            new Pose2d(4.75, -0.2, Rotation2d.fromDegrees(0)),
+            Constants.AutoConstants.forwardConfig);
+
+    public static Trajectory returnPath_1_Blue = TrajectoryGenerator.generateTrajectory(
+            new Pose2d(4.75, -0.2, Rotation2d.fromDegrees(0)),
+            List.of(new Translation2d(1.2, 0)),
+            new Pose2d(0.3, -0.6, Rotation2d.fromDegrees(0)),
+            Constants.AutoConstants.reverseConfig);
+
+    public static Trajectory middlePath_2_Blue = TrajectoryGenerator.generateTrajectory(
             new Pose2d(0, 0, Rotation2d.fromDegrees(0)),
             List.of(new Translation2d(1, 0)),
             new Pose2d(5, 0, Rotation2d.fromDegrees(0)),
