f0ff0052d2105ab2dfe52ce809ff3c1f0483e8eb
maisy thomas <maisy0404@outlook.com>
2023-03-27 17:02:43 -0400

    update blue side

--- src/main/java/frc/robot/autos/TwoPiecePurpleBarrier.java
+++ src/main/java/frc/robot/autos/TwoPiecePurpleBarrier.java
@@ -79,13 +79,10 @@ public class TwoPiecePurpleBarrier extends SequentialCommandGroup {
             addCommands(new ResetRobotPose(container.getSwerve(), middlePath_1_Blue.getInitialPose()));
 
             // Pivot and extend arm
-            addCommands(new PivotMoveToPosition(container.getPivot(), Constants.PivotConstants.kPositionScoringCone));
+            addCommands(new PivotMoveToPosition(container.getPivot(), Constants.PivotConstants.kPositionScoringCube));
             addCommands(
-                    new TelescopicScoringExtendFar(container.getTelescopic(), container.getPivot()).withTimeout(1));
-
-            // drop pivot and open gripper
-            addCommands(
-                    new PivotMoveToPosition(container.getPivot(), Constants.PivotConstants.kPositionScoringRelease));
+                    new TelescopicScoringExtendFar(container.getTelescopic(), container.getPivot()).withTimeout(.55));
+            // Open gripper
             addCommands(new GripperRelease(container.getGripper())
                     .withTimeout(Constants.GripperConstants.kGripperReleaseTimeout));
 
@@ -111,7 +108,7 @@ public class TwoPiecePurpleBarrier extends SequentialCommandGroup {
 
             // extend arm
             addCommands(
-                    new TelescopicScoringExtendFar(container.getTelescopic(), container.getPivot()).withTimeout(1.0));
+                    new TelescopicScoringExtendMid(container.getTelescopic(), container.getPivot()).withTimeout(1.0));
             // Drop score, and retract
             addCommands(new SequentialCommandGroup(new Command[] {
                     // new PivotMoveToPosition(container.getPivot(),
@@ -123,6 +120,15 @@ public class TwoPiecePurpleBarrier extends SequentialCommandGroup {
                             new PivotMoveToPosition(container.getPivot(), Constants.PivotConstants.kPositionTravel)
                                     .withTimeout(1.0),
                             AutoFollowPath.createFollowCommand(container.getSwerve(), middlePath_2_Blue) }) }));
+
+            addCommands(new ParallelCommandGroup(new Command[] {
+                    new GripperRelease(container.getGripper())
+                            .withTimeout(Constants.GripperConstants.kGripperReleaseTimeout),
+                    new ConeGuideDeploy(container.getConeGuide())
+                            .withTimeout(Constants.ConeGuideConstants.kConeGuideRetractTimeout),
+                    new PivotMoveToPosition(container.getPivot(), Constants.PivotConstants.kPositionPickupCone)
+            }));
+            ;
         }
     }
 
@@ -152,20 +158,19 @@ public class TwoPiecePurpleBarrier extends SequentialCommandGroup {
     /* ========== */
     public static Trajectory middlePath_1_Blue = TrajectoryGenerator.generateTrajectory(
             new Pose2d(0, 0, Rotation2d.fromDegrees(0)),
-            List.of(new Translation2d(2, 0)),
-            new Pose2d(4.75, -0.381, Rotation2d.fromDegrees(0)),
-            Constants.AutoConstants.forwardConfig);
+            List.of(),
+            new Pose2d(4.65, 0.2, Rotation2d.fromDegrees(0)),
+            Constants.AutoConstants.twoPurpleBarrierForwardConfig);
 
     public static Trajectory returnPath_1_Blue = TrajectoryGenerator.generateTrajectory(
-            new Pose2d(4.75, -0.381, Rotation2d.fromDegrees(0)),
-            List.of(new Translation2d(1.2, 0)),
-            new Pose2d(0.3, -0.6, Rotation2d.fromDegrees(0)),
-            Constants.AutoConstants.reverseConfig);
+            new Pose2d(4.65, 0.2, Rotation2d.fromDegrees(0)),
+            List.of(),
+            new Pose2d(0, 0, Rotation2d.fromDegrees(0)),
+            Constants.AutoConstants.twoPurpleBarrierReverseConfig);
 
     public static Trajectory middlePath_2_Blue = TrajectoryGenerator.generateTrajectory(
             new Pose2d(0, 0, Rotation2d.fromDegrees(0)),
-            List.of(new Translation2d(1, 0)),
-            new Pose2d(5, 0, Rotation2d.fromDegrees(0)),
-            Constants.AutoConstants.forwardConfig);
-
+            List.of(new Translation2d(5.5, 0)),
+            new Pose2d(6, -1, Rotation2d.fromDegrees(0)),
+            Constants.AutoConstants.twoPurpleBarrierForwardConfigFast);
 }
