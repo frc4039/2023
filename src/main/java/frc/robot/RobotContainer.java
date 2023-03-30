f56ccca756a0e1270f3e578109dcbab039ad1c43
Pablo Cafiso <pablo.cafiso@verticalmotion.ca>
2023-03-30 17:04:47 -0400

    Minor position adjustment

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -134,10 +134,10 @@ public class RobotContainer {
         mainTab.addDouble("Gyro", () -> s_Swerve.getYaw().getDegrees()).withSize(1, 1)
                 .withPosition(3, 0);
         mainTab.add("AutoMode", autoModeSelector.getAutoChooser()).withSize(2, 1).withPosition(0, 1);
-        mainTab.add("Gyro zero", new ZeroGyro(s_Swerve)).withSize(2, 1).withPosition(3, 1);
+        mainTab.add("Gyro zero", new ZeroGyro(s_Swerve)).withSize(2, 1).withPosition(2, 1);
         mainTab.addDouble("Analog Pressure Sensor", () -> PressureSensor.getAnalogPressureReading()).withSize(2, 1)
                 .withPosition(0, 2);
-        mainTab.add("Reset angle encoders", new ResetSwerveAngleEncoders(s_Swerve)).withSize(2, 1).withPosition(3, 2);
+        mainTab.add("Reset angle encoders", new ResetSwerveAngleEncoders(s_Swerve)).withSize(2, 1).withPosition(2, 2);
         mainTab.add("Game Field", new Field2d()).withSize(5, 3).withPosition(4, 0);
 
         configureButtonBindings();
