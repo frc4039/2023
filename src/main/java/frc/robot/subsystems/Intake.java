ae3b70ab3010682b4f12e647376be0083848f929
Pablo Cafiso <pablo.cafiso@verticalmotion.ca>
2023-04-13 16:35:41 -0400

    Minor adjustment to the Intake encoder resetting.

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -85,17 +85,17 @@ public class Intake extends SubsystemBase {
 
     public void intakeBreak() {
         m_intakeMotorRight.set(IntakeConstants.kBreakSpeed);
-        if (m_integratedIntakeEncoderRight.getPosition() < 0) {
-            m_integratedIntakeEncoderRight.setPosition(0);
-        }
         m_intakeMotorLeft.set(IntakeConstants.kBreakSpeed);
-        if (m_integratedIntakeEncoderLeft.getPosition() < 0) {
-            m_integratedIntakeEncoderLeft.setPosition(0);
-        }
     }
 
     @Override
     public void periodic() {
+        if (m_integratedIntakeEncoderRight.getPosition() < 0) {
+            m_integratedIntakeEncoderRight.setPosition(0);
+        }
+        if (m_integratedIntakeEncoderLeft.getPosition() < 0) {
+            m_integratedIntakeEncoderLeft.setPosition(0);
+        }
     }
 
     private void ConfigIntakeMotor(CANSparkMax motor, boolean invertedMode, RelativeEncoder integratedEncoder,
