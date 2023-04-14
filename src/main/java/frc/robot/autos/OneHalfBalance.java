ed1a719bdbce3226ba138c337cc2ba77652ad826
Noah Linton <nlinton123@hotmail.com>
2023-04-14 16:15:32 -0400

    added timeout and adjusted pickup positions

--- src/main/java/frc/robot/autos/OneHalfBalance.java
+++ src/main/java/frc/robot/autos/OneHalfBalance.java
@@ -42,7 +42,7 @@ public class OneHalfBalance extends SequentialCommandGroup {
                         new SeqCmdCubePickupPosition(container.getTelescopic(),
                                 container.getConeGuide(),
                                 container.getGripper(), container.getIntake(), container.gIntakeSpinner(),
-                                container.getPivot())
+                                container.getPivot()).withTimeout(4)
                 }),
                 AutoFollowPath.createFollowCommand(container.getSwerve(),
                         pDropToMobility)
@@ -78,11 +78,11 @@ public class OneHalfBalance extends SequentialCommandGroup {
     public static Trajectory pDropToMobility = TrajectoryGenerator.generateTrajectory(
             new Pose2d(0, 0, Rotation2d.fromDegrees(0)),
             List.of(),
-            new Pose2d(5.2, 0, Rotation2d.fromDegrees(0)),
+            new Pose2d(5.5, 0, Rotation2d.fromDegrees(0)),
             Constants.AutoConstants.chargeStationForwardConfigHalfGamepiece);
 
     public static Trajectory pPickupToCharge = TrajectoryGenerator.generateTrajectory(
-            new Pose2d(5.2, 0, Rotation2d.fromDegrees(0)),
+            new Pose2d(5.5, 0, Rotation2d.fromDegrees(0)),
             List.of(),
             new Pose2d(4.25, 0, Rotation2d.fromDegrees(0)),
             Constants.AutoConstants.chargeStationReverseConfigHhalfGamepiece);
